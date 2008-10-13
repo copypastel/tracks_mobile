9ed7626711932bbc6048b41d676206c30241a00b
daicoden <daicoden@rpimshptc4400.(none)>
2008-10-13 10:22:23 -0400

    Setting up git ignore and base file with git and svn to see how it works...

---
+++ src/API/Connection.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package API;
+
+/**
+ *
+ * @author daicoden
+ */
+
+public class Connection {
+
+}
